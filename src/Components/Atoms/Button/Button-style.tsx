bb8e1b3819f219f4a2482e16f5dc3aa7913ccfd3
Łukasz Żurawski <zurawskiu@gmail.com>
2021-04-29 18:18:19 +0200

    simple hover added

--- src/Components/Atoms/Button/Button-style.tsx
+++ src/Components/Atoms/Button/Button-style.tsx
@@ -12,8 +12,12 @@ height: 3em;
 background: #30B0F8;
 box-shadow: 9px 4px 17px 4px rgba(0,0,0,0.21);
 border: none;
-border-radius: 26.5px;
 color: #fff;
 font-size: 20px;
 font-family: Merriweather Sans;
+cursor: pointer;
+transition: .3s linear;
+&:hover {
+   border-radius: 26.5px;
+}
 `;
